962f9a2c15b397c1e90b2602c41992c5ea4310fc
Ashkar S <ashkars178@gmail.com>
2025-11-28 09:32:43 +0530

    doing

--- controller/dummyController.js
+++ controller/dummyController.js
@@ -1,6 +1,4 @@
 
-
-
 exports.getDetails=(req, res) => {
     res.status(200).json({ message: "recived" });
   }
--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const express = require("express");
-const cors=require('cors');
+const cors = require("cors");
 const router = require("./routes");
-
-
-
 
 const server = new express();
 
 const PORT = 3000;
 
-
 //midlleware to allow rrsource sharing btwn origins
-server.use(cors())
+server.use(cors());
 
 //midlleWare to parse
 server.use(express.json());
 
-
-server.use(router)
-
-
+server.use(router);
 
 server.listen(PORT, () => {
   console.log(PORT);
